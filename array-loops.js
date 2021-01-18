adaca40ed61586cdcca186429f30e9c3ffa48c4b
Samir Gasjimov <kingsem@hotmail.com>
2021-01-18 03:54:46 +0100

    Array&Loops opdracht afgerond

--- array-loops.js
+++ array-loops.js
@@ -2,31 +2,40 @@
 // Opdracht 1
 // --------------------------
 
-const names = ["Henk", "Piet", "Fred", "Joop"];
+let names = ["Henk", "Piet", "Fred", "Joop"];
+names.forEach(function (array) {
+    console.log([array + 'je'])
+});
 
 // Verander de waardes van de array dusdanig dat er -"je" achter iedere naam wordt geplakt
 // Wanneer je de volledige array logt zie je alle nieuwe waardes
 // console.log(names) geeft: ["Henkje", "Pietje", "Fredje", "Joopje"]
 
 
-
 // --------------------------
 // Opdracht 2
 // --------------------------
 
-const numbers = [2, 4, 5, 29, 38];
+let numbers = [2, 4, 5, 29, 38];
+let multiply = 3;
+numbers.forEach(function (value) {
+    console.log([value * multiply])})
+
+
 
 // Verander de waardes van de array dusdanig dat iedere waarde met 3 vermenigvuldigd wordt
 // Wanneer je de volledige array logt zie je alle nieuwe waardes
 // console.log(numbers) geeft: [ 6, 12, 15, 87, 114 ];
 
 
-
 // --------------------------
 // Opdracht 3
 // --------------------------
-
 const figures = [4, 6, 12, 34, 1];
+let add = 3;
+figures.forEach(function (index ) {
+    console.log( [index + add])})
+
 
 // Verander de waardes van de array dusdanig dat er 3 bij iedere waarde wordt opgetelt
 // Wanneer je de volledige array logt zie je alle nieuwe waardes
